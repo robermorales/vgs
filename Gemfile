0b553e53f2f42ed625d9f60e28eb9a83084e496e
Rober Morales-Chaparro <rober.morales@gmail.com>
2012-02-27 00:02:18 +0100

    pg

--- Gemfile
+++ Gemfile
@@ -3,3 +3,9 @@ source :rubygems
 gem 'sinatra'
 gem 'thin'
 gem 'foreman'
+gem 'haml'
+gem 'sass'
+gem 'dm-core'
+gem 'dm-validations'
+gem 'dm-postgres-adapter'
+
--- web.rb
+++ web.rb
@@ -1,6 +1,17 @@
+require 'rubygems'
 require 'sinatra'
+require 'dm-core'
+require 'dm-validations'
+require 'logger'
 
 get '/' do
+	haml :layout
 	'Hello world!'
+end
+
+# SASS stylesheet
+get '/stylesheets/style.css' do
+  header 'Content-Type' => 'text/css; charset=utf-8'
+  sass :style
 end
 
